14aec672af97d2ed48706a1a1eba0c5176b1dc93
Bruno Matheus Pereira Silva <82674117+bruno9800@users.noreply.github.com>
2023-05-31 21:38:51 -0300

    prepare for production

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -5,9 +5,7 @@ import { knex } from '../database';
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.addHook('preHandler', async (request, reply) => {
-    console.log('[${}]');
-  });
+  app.addHook('preHandler', checkSessionIdExists);
 
   app.get(
     '/',
